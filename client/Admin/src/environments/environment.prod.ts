708ae97834b0336bce6eb0997bb11026dfb9e32e
EC2 Default User <ec2-user@ip-172-31-84-48.ec2.internal>
2025-06-03 20:26:32 +0000

    Viento en popa, todo OK

--- client/Admin/src/environments/environment.prod.ts
+++ client/Admin/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tdsplee303.execute-api.us-east-1.amazonaws.com/prod',
+  apiUrl: 'https://v8z5vu16lb.execute-api.us-east-1.amazonaws.com/prod',
 };
--- client/Application/src/environments/environment.prod.ts
+++ client/Application/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
-export const environment = {
-  production: true,
-  regApiGatewayUrl:
-    'https://duobieudcl.execute-api.us-west-2.amazonaws.com/prod',
-};
+  export const environment = {
+    production: true,
+    regApiGatewayUrl: 'https://v8z5vu16lb.execute-api.us-east-1.amazonaws.com/prod'
+  };
