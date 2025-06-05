f250520bfd58fbb57eea4e7b42e2b7ec7ced4c96
EC2 Default User <ec2-user@ip-172-31-84-48.ec2.internal>
2025-06-05 00:22:50 +0000

    ec2 to git

--- client/Admin/src/aws-exports.ts
+++ client/Admin/src/aws-exports.ts
@@ -1,8 +1,8 @@
 const awsmobile = {
     "aws_project_region": "us-east-1",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_HiaV0afyz",
-    "aws_user_pools_web_client_id": "28evcahs7ekfdacbh78nruui6a",
+    "aws_user_pools_id": "None",
+    "aws_user_pools_web_client_id": "None",
 };
 
 export default awsmobile;
--- client/Application/src/environments/environment.prod.ts
+++ client/Application/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
   export const environment = {
     production: true,
-    regApiGatewayUrl: 'https://v8z5vu16lb.execute-api.us-east-1.amazonaws.com/prod'
+    regApiGatewayUrl: 'None'
   };
