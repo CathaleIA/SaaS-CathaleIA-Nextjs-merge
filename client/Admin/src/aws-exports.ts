a31acce8d0b5d1b12345845d76b1cecd245aabc8
EC2 Default User <ec2-user@ip-172-31-84-48.ec2.internal>
2025-06-18 20:23:59 +0000

    EC2 Last update

--- client/Admin/src/aws-exports.ts
+++ client/Admin/src/aws-exports.ts
@@ -1,8 +1,8 @@
 const awsmobile = {
     "aws_project_region": "",
     "aws_cognito_region": "",
-    "aws_user_pools_id": "us-east-1_sPpuvMlPj",
-    "aws_user_pools_web_client_id": "1fkblelf9fnvjdqm5gcqqutlo5",
+    "aws_user_pools_id": "us-east-1_Prk7hNrVk",
+    "aws_user_pools_web_client_id": "1as4evace55nsj4jfc968akps2",
 };
 
 export default awsmobile;
--- client/Admin/src/environments/environment.ts
+++ client/Admin/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://imv9vbdwp0.execute-api.us-east-1.amazonaws.com/prod',
+  apiUrl: 'https://xvi8lgw3z4.execute-api.us-east-1.amazonaws.com/prod',
 };
