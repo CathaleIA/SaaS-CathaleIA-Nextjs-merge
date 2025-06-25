2b84d3d71d4bf2727ae205101804d02ca2e07e1a
HaderCabrera <hader.cabrera96@gmail.com>
2025-06-25 17:43:53 -0500

    funcion python

--- scripts/deployment-reports.sh
+++ scripts/deployment-reports.sh
@@ -1,3 +1,38 @@
-cd ../server
+if [[ "$#" -eq 0 ]]; then
+  echo "Invalid parameters"
+  echo "Command to deploy normalizar function: deployment.sh -n"
+  echo "Command to deploy generatePDF function: deployment.sh -g"
+  echo "Command to deploy descargar function: deployment.sh -d"
+  exit 1      
+fi
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -n) normalizar=1 ;;
+        -g) crear=1 ;;
+        -d) descargar=1 ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
 REGION=$(echo "us-east-1")
-sam deploy --template tenant-reports.yaml --config-file tenant-reports.toml --region=$REGION
+
+if [[ $normalizar -eq 1 ]] || [[ $crear -eq 1 ]]; then
+  echo "Desplegando funciones java"
+
+  cd ../server
+  sam deploy --template tenant-reports.yaml --config-file tenant-reports.toml --region=$REGION
+
+  cd ../scripts
+fi
+
+
+if [[ $normalizar -eq 1 ]]; then
+  echo "Desplegando funciones java"
+
+  cd ../server
+  sam deploy --template tenant-reports.yaml --config-file tenant-reports.toml --region=$REGION
+
+  cd ../scripts
+fi
